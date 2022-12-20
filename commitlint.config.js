1ab8dfd8642690b23f29a517e6b1b6b8bd634727
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2022-12-21 00:13:00 +1000

    chore(root): setup gitignore and pre-commit hooks

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build', // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+                'chore', // Other changes
+                'ci', // Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+                'docs', // Documentation only changes
+                'feat', // A new feature
+                'fix', // A bug fix
+                'perf', // A code change that improves performance
+                'refactor', // A code change that neither fixes a bug nor adds a feature
+                'revert', // A commit used to revert a previous commit
+                'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+                'test', // Adding missing tests or correcting existing tests
+            ],
+        ],
+    },
+    helpUrl: "https://github.com/conventional-changelog/commitlint/#what-is-commitlint"
+};
