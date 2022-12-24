dd10d0a61be74825236ee840cc56bbff3ed6183d
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2022-12-24 13:33:05 +1000

    refactor(api): changed user_id to id

--- api/domain/src/models/user.rs
+++ api/domain/src/models/user.rs
@@ -3,7 +3,7 @@ use rocket::serde::uuid::Uuid;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
-    user_id: Uuid,
+    id: Uuid,
     email: String,
     hashed_password: String,
     settings: UserSettings,
